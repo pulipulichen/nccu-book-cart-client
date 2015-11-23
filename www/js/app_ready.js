d4ca139f276c4d56c0f249d98dbfffc59907c533
Pulipuli Chen <pulipuli.chen@gmail.com>
2015-11-23 11:12:26 +0800

    close #17 分離完成

--- www/js/app_ready.js
+++ www/js/app_ready.js
@@ -3,6 +3,7 @@ var _app_ready = function ($scope) {
     ons.ready(function () {
         $scope.load_todo_list();
         $scope.setup_menu_swipeable();
+        //alert($(window).width())
     });
 
 };
